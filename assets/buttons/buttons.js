60ab32e9e1254a84e8a43b6d69ae54a3a2fd8ae0
Francy <francyegr@gmail.com>
2018-08-10 10:44:51 -0700

    Added changes to buttons.js

--- assets/buttons/buttons.js
+++ assets/buttons/buttons.js
@@ -1,1 +1,11 @@
-var button = {};
+var fn = function() {
+  var minus = document.getElementsByClassName('minus');
+
+  for (var i = 0; i< minus.lenght; i++) {
+    var e = document.createElement('p');
+    e.innerHTML = '-';
+    minus[i].appendChild(e);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', fn, false);
