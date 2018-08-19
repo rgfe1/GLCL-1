130dfec9b6fb5c73c3c29a84036b2d421b1c164c
Francy <francyegr@gmail.com>
2018-08-19 09:16:25 -0700

    added JavaScript files for the tracking page and stiled the tracking page

--- assets/buttons/buttons.js
+++ assets/buttons/buttons.js
@@ -1,11 +1,11 @@
-var fn = function() {
+
+var fn = function(){
   var minus = document.getElementsByClassName('minus');
 
-  for (var i = 0; i< minus.lenght; i++) {
+  for(var i=0; i<minus.length;i++){
     var e = document.createElement('p');
     e.innerHTML = '-';
     minus[i].appendChild(e);
   }
 };
-
 document.addEventListener('DOMContentLoaded', fn, false);
--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -3,13 +3,12 @@
 var fn = function fn() {
   var minus = document.getElementsByClassName('minus');
 
-  for (var i = 0; i < minus.lenght; i++) {
+  for (var i = 0; i < minus.length; i++) {
     var e = document.createElement('p');
     e.innerHTML = '-';
     minus[i].appendChild(e);
   }
 };
-
 document.addEventListener('DOMContentLoaded', fn, false);
 'use strict';
 
