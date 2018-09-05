192f441c851449fd12776f21c70b3b77218e09ac
Francy <francyegr@gmail.com>
2018-09-04 19:45:34 -0700

    Changes to the dist folder; css and scripts subfolders;

--- dist/scripts/scripts.js
+++ dist/scripts/scripts.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var fn = function fn() {
+	// adding p tag for minus
 	var minus = document.getElementsByClassName('minus');
 
 	for (var i = 0; i < minus.length; i++) {
@@ -8,9 +9,68 @@ var fn = function fn() {
 		e.innerHTML = '-';
 		minus[i].appendChild(e);
 	}
+
+	//adding p tag for plus
+	var plus = document.getElementsByClassName('plus');
+	for (var i = 0; i < plus.length; i++) {
+		var e = document.createElement('p');
+		e.innerHTML = '+';
+		plus[i].appendChild(e);
+	}
 };
 
 document.addEventListener('DOMContentLoaded', fn, false);
+'use strict';
+
+var calendar = function calendar() {
+  var lastDay = function lastDay(year, month) {
+    return new Date(year, month, 0).getDate();
+  };
+  var firstDay = function firstDay(year, month) {
+    return new Date(year, month - 1, 1).getDay();
+  };
+
+  var daysOfTheWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  var calendarHead = document.getElementById('calendar-head');
+  var populateCalendarHead = function populateCalendarHead() {
+    for (var i = 0; i < daysOfTheWeek.length; i++) {
+      var day = document.createElement('p');
+      day.innerHTML = daysOfTheWeek[i];
+      calendarHead.appendChild(day);
+    }
+  };
+  populateCalendarHead();
+
+  var calendarBody = document.getElementById('calendar-body');
+  var populateCalendarBody = function populateCalendarBody() {
+    var daysInRow = 7;
+    var totalRows = 5;
+    // var totalDays = daysInRow * totalRows;
+    var currentRows = 0;
+
+    while (currentRows < totalRows) {
+      var calendarRow = document.createElement('div');
+      for (var i = 0; i < daysInRow; i++) {
+        console.log('within for loop');
+        var day = document.createElement('p');
+        day.innerHTML = i;
+        calendarRow.appendChild(day);
+      }
+      calendarBody.appendChild(calendarRow).className = "calendar-row";
+      currentRows++;
+    }
+  };
+  populateCalendarBody();
+};
+
+document.addEventListener('DOMContentLoaded', calendar, false);
+
+//lastDay(2018, 9);//october 1st if using 1 as a day
+//firstDay(2018, 9);//september 30th if using 0 as a day
+//to see the result in the console
+// let lastDayOfSept = lastDay(2018, 9);
+// let
+// console.log('lastDayOfSept', lastDayOfSept);
 'use strict';
 
 var list = [];
